0c59b34fc05a9fb3d18ac1e482d26fea6fe7faee
kleinarthu <arthur.klein@eisti.fr>
2023-03-01 15:57:58 +0100

    Initial commit

---
+++ Generator.ml
@@ -0,0 +1,110 @@
+module Generator :
+  sig
+    (** Type du générateur pseudo-aléatoire de données de type 'a *)
+    type 'a t
+
+    (** Renvoie une nouvelle valeur aléeatoire
+      * @param gen générateur pseudo-aléatoire
+      * @return    nouvelle valeur aléatoire en utilisant `gen`
+      *)
+    val next : 'a t -> 'a
+
+    (** Générateur constant
+      * @param x valeur
+      * @return  générateur de l'unique valeur `x`
+      *)
+    val const : 'a -> 'a t
+
+    (* GENERATEURS DE TYPES DE BASE *)
+ 
+    (** Générateur pseudo-aléatoire de booléens
+      * @param prob probabilité de la valeur `true`
+      * @return     générateur pseudo-aléatoire de valeurs booléennes
+      *)
+    val bool : float -> bool t
+
+    (** Générateur pseudo-aléatoire d'entiers
+      * @param a borne inférieure
+      * @param b borne supérieure
+      * @return  générateur pseudo-aléatoire de valeurs entières entre `a` et `b` inclus
+      *)
+    val int : int -> int -> int   t
+
+    (** Générateur pseudo-aléatoire d'entiers positifs ou nuls
+      * @param n borne supérieure
+      * @return  générateur pseudo-aléatoire de valeurs entières entre 0 et `n` inclus
+      *)
+    val int_nonneg : int -> int   t
+
+    (** Générateur pseudo-aléatoire de flottants
+      * @param x borne supérieure
+      * @param y borne supérieure
+      * @return  générateur pseudo-aléatoire de valeurs flottantes entre `x` et `y` inclus
+      *)
+    val float : float -> float -> float t
+
+    (** Générateur pseudo-aléatoire de flottants positifs ou nuls
+      * @param x borne supérieure
+      * @return  générateur pseudo-aléatoire de valeurs flottantes entre 0 et `x` inclus
+      *)
+    val float_nonneg : float -> float t
+
+    (** Générateur pseudo-aléatoire de caractères *)
+    val char : char t
+
+    (** Générateur pseudo-aléatoire de caractères alphanumériques *)
+    val alphanum : char t
+
+    (* GENERATEURS DE CHAINE DE CARACTERE *)
+
+    (** Générateur de chaînes de caractères
+      * @param n   longueur maximale de la chaîne de caractère
+      * @param gen générateur pseudo-aléatoire de caractères
+      * @return    générateur pseudo-aléatoire de chaînes de caractères dont chaque caractéré est généré avec `gen`
+      *)
+    val string : int -> char t -> string t
+
+    (* GENERATEURS DE LISTES *)
+
+    (** Générateur de listes
+      * @param n   longueur maximale de la liste
+      * @param gen générateur pseudo-aléatoire d'éléments
+      * @return    générateur pseudo-aléatoire de listes dont chaque élément est généré avec `gen`
+      *)
+    val list : int -> 'a t -> ('a list) t
+
+    (* TRANSFORMATIONS *)
+
+    (** Générateur pseudo-aléatoire de couples
+      * @param fst_gen générateur pseudo-aléatoire de la première coordonnée
+      * @param snd_gen générateur pseudo-aléatoire de la deuxième coordonnée
+      * @return        générateur pseudo-aléatoire du couple
+      *)
+    val combine : 'a t -> 'b t -> ('a * 'b) t
+
+    (** Applique un post-traitement à un générateur pseudo-aléatoire
+      * @param f   post-traitement à appliquer à chaque valeur générée
+      * @param gen générateur pseudo-aléatoire
+      * @return    générateur pseudo-aléatoire obtenu en appliquant `f` à chaque valeur générée par `gen`
+      *)
+    val map : ('a -> 'b) -> 'a t -> 'b t
+
+    (** Applique un filtre à un générateur pseudo-aléatoire
+      * @param p   filtre à appliquer à chaque valeur générée
+      * @param gen générateur pseudo-aléatoire
+      * @return    générateur pseudo-aléatoire ne générant des valeurs de `gen` que si elles vérifient `p`
+      *)
+    val filter : ('a -> bool) -> 'a t -> 'a t
+
+    (** Applique un post-traitement dépendant d'un filtre à un générateur pseudo-aléatoire
+      * @param p   filtre à appliquer à chaque valeur générée
+      * @param f   couple des post-traitements à appliquer à chaque valeur générée
+      * @param gen générateur pseudo-aléatoire
+      * @return    générateur pseudo-aléatoire obtenu en appliquant `fst f` pour toute valeur vérifiant `p`
+      *                                                          et `snd f` pour toute valeur ne le vérifiant pas
+      *)
+    val partitioned_map : ('a -> bool) -> (('a -> 'b) * ('a -> 'b)) -> 'a t -> 'b t
+  end =
+  struct
+    (* TODO : Implémenter le type et tous les éléments de la signature *)
+  end ;;
---
+++ Reduction.ml
@@ -0,0 +1,90 @@
+module Reduction :
+  sig
+    (** Type d'une stratégie de réduction des éléments de type 'a
+      * Une stratégie associe à chaque valeur une liste de propositions plus "simples".
+      * NB : Les propositions sont ordonnées dans l'ordre croissance de "simplicité"
+      *      (i.e. les valeurs les plus "simples" sont en début de liste).
+      * IMPORTANT : Les stratégies implémentées respectent les conditions des générateurs correspondants.
+      *)
+    type 'a t = 'a -> 'a list
+
+    (** La stratégie vide : ne renvoie aucune proposition de réduction *)
+    val empty : 'a t
+
+    (* TYPES DE BASE *)
+
+    (** Stratégie de réduction sur les entiers
+      * @param n entier
+      * @return  liste d'entiers plus "simples" entre `-|n|` et `|n|`
+      *)
+    val int : int t
+
+    (** Stratégie de réduction sur les entiers positifs
+      * @param n entier positif
+      * @return  liste d'entiers naturels plus "simples" entre 0 et `n`
+      *)
+    val int_nonneg : int t
+
+    (** Stratégie de réduction sur les flottants
+      * @param x flottant
+      * @return  liste de flottants plus "simples" entre `-|x|` et `|x|`
+      *)
+    val float : float t
+
+    (** Stratégie de réduction sur les flottants positifs
+      * @param x flottant positif
+      * @return  liste de flottants positifs plus "simples" entre `0` et `x`
+      *)
+    val float_nonneg : float t
+
+    (** Stratégie de réduction sur les caractères
+      * @param c caractère
+      * @return  liste de caractères plus "simples"
+      *)
+    val char : char t
+
+    (** Stratégie de réduction sur les caractères alphanumériques
+      * @param c caractère alphanumérique
+      * @return  liste de caractères alphanumériques plus "simples"
+      *)
+    val alphanum : char t
+
+    (* CHAINES DE CARACTERES *)
+
+    (** Stratégie de réduction sur les chaînes de caractères
+      * @param red stratégie de réduction à utiliser sur chaque caractère
+      * @param s   chaîne de caractères
+      * @return    liste de chaînes de caractères plus "simples" au pire aussi longues que `s`
+      *)
+    val string : char t -> string t
+
+    (* LISTES *)
+
+    (** Stratégie de réduction sur les listes
+      * @param red stratégie de réduction à utiliser sur chaque élément
+      * @param l   liste
+      * @return    liste de listes plus "simples" au pire aussi longues que `l`
+      *)
+    val list : 'a t -> ('a list) t
+
+    (* TRANSFORMATIONS *)
+
+    (** Stratégie de réduction sur les couples
+      * @param fst_red stratégie de réduction de la première coordonnée
+      * @param snd_red stratégie de réduction de la deuxième coordonnée
+      * @return        stratégie de réduction sur les couples correspondants
+      *)
+    val combine : 'a t -> 'b t -> ('a * 'b) t
+
+    (** Applique un filtre à une stratégie de réduction
+      * @param p   filtre à appliquer à chaque réduction
+      * @param red stratégie de réduction
+      * @return    stratégie de réduction ne contenant que des propositions vérifiant `p`
+      *)
+    val filter : ('a -> bool) -> 'a t -> 'a t
+  end =
+  struct
+    type 'a t = 'a -> 'a list ;;
+
+    (* TODO : Implémenter tous les éléments de la signature manquants *)
+  end ;;
