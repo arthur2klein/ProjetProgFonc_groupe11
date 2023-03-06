fe090531f7e9614e4f5eb16b34050618ce825dd6
Assia Mighis <assia.mighis@eisti.fr>
2023-03-06 16:09:20 +0100

    Update Test.ml

--- Test.ml
+++ Test.ml
@@ -42,11 +42,11 @@ module Test :
        generator : 'a Generator.t;
        reduction : 'a Reduction.t;
        property  : 'a Property.t
-    }
+    } ;;
 
 
     let make_test (g : 'a Generator.t) (r :'a Reduction.t) (p :'a Property.t) : 'a t =
-       { generator = g; reduction = r; property = p }
+       { generator = g; reduction = r; property = p } ;;
       
        
     let generate_reduce_verify (test: 'a t): 'a option =
@@ -54,7 +54,7 @@ module Test :
        let liste_red = test.reduction val_gen in 
        let liste_red_satisfy_property = List.for_all test.property liste_red in 
        if liste_red_satisfy_property then Some val_gen 
-       else None 
+       else None ;;
 
     
     let check (n : int) (test : 'a t) : bool =  
@@ -67,7 +67,7 @@ module Test :
                    | Some _ -> boucle (n-1)
                    | None -> false
           in 
-          boucle n
+          boucle n ;;
 
 
 
